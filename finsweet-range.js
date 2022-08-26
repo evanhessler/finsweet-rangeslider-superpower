79167d7c2ae64cd22202be0a76bcad1e690d1f7e
Evan Hessler <ehessler@lion.lmu.edu>
2022-08-25 21:23:56 -0700

    Add files via upload

--- finsweet-range.js
+++ finsweet-range.js
@@ -499,13 +499,13 @@
                 console.log(n);
                 // console.log(element);
                 console.log(this.element);
-                // if (element && element.get_attribute('fs-rangeslider-element-symbol')) {
-                //     symbolValue = n.get_attribute('fs-rangeslider-element-symbol')
-                // }
-                // else {
-                //     symbolValue = "";
-                // }
-                // d = symbolValue + d;
+                if (this.element && this.element.get_attribute('fs-rangeslider-element-symbol')) {
+                    symbolValue = this.element.get_attribute('fs-rangeslider-element-symbol');
+                }
+                else {
+                    symbolValue = "";
+                }
+                d = symbolValue + d;
                 if (t <= minRange && t != 0) {
                     d = "< " + d
                 }
